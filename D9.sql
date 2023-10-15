94707b2e6b801e4f398d602ebf2b61b3c7ee7dc5
Axel <111228016+viti1igo@users.noreply.github.com>
2023-10-16 03:49:47 +1100

    Update D9.sql

--- D9.sql
+++ D9.sql
@@ -1,5 +1,8 @@
 --Q1--
-
+SELECT
+SUM(CASE WHEN device_type = 'laptop' THEN 1 ELSE 0 END) AS laptop_views,
+SUM(CASE WHEN device_type = 'tablet' OR device_type = 'phone' THEN 1 ELSE 0 END) AS tablet_views
+FROM viewership
 --Q2--
 SELECT *
 CASE WHEN x+y>z AND x+z>y AND y+z>x THEN 'Yes'
